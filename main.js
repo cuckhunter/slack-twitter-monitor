7f09a7625cc266ced5519dff33742780eb34446b
Chiraag Mundhe <cmundhe@gmail.com>
2016-12-07 00:53:40 -0500

    Refactor and web server

---
+++ main.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const lib = require('./lib');
+const Promise = require('bluebird');
+
+function main() {
+
+  const logger = lib.getLogger();
+  const twitterClient = lib.getTwitterClient(logger);
+  const slackClient = lib.getSlackClient(logger);
+  const server = lib.getServer(logger, slackClient);
+
+  Promise.try(() => {
+
+    return twitterClient.getUserTimeline({
+      screen_name: 'realDonaldTrump',
+      count: 1
+    });
+
+  }).then((data) => {
+
+    /*test*/ slackClient.publish(data, logger);
+
+    return poll(data[0].id_str, logger, slackClient, twitterClient);
+
+  }).catch((err) => {
+
+    logger.error(err, Array.from(arguments).slice(1));
+
+  });
+
+  // Start server
+  server.start();
+
+}
+
+function poll(sinceId, logger, slackClient, twitterClient) {
+
+  const POLL_INTERVAL = 10 * 1000;
+
+  return Promise.try(() => {
+
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, POLL_INTERVAL);
+    });
+
+  }).then(() => {
+
+    logger.info('polling');
+
+    return twitterClient.getUserTimeline({
+      screen_name: 'realDonaldTrump',
+      since_id: sinceId
+    });
+
+  }).then((data) => {
+
+    if (data.length) {
+      sinceId = data[0].id_str;
+      return slackClient.publish(data, logger);
+    }
+
+  }).then(() => {
+    
+    return poll(sinceId, logger, slackClient, twitterClient);
+
+  });
+
+}
+
+main();
