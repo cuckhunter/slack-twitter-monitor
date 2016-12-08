898e54d91a08b85a954e3701368dc8a5122a1ef8
Chiraag Mundhe <cmundhe@gmail.com>
2016-12-07 21:29:05 -0500

    Async/await refactor

--- main.js
+++ main.js
@@ -4,75 +4,65 @@
 
 require('dotenv').config();
 const lib = require('./lib');
-const Promise = require('bluebird');
 
 const TWITTER_SCREEN_NAME = 
 'realDonaldTrump'
 //'notiven'
 ;
 
-function main() {
+async function main() {
 
   const logger = lib.getLogger();
   const twitterClient = lib.getTwitterClient(logger);
   const slackClient = lib.getSlackClient(logger);
   const server = lib.getServer(logger, slackClient);
 
-  Promise.try(() => {
-
-    return twitterClient.getUserTimeline({
+  try {
+ 
+    const data = await twitterClient.getUserTimeline({
       screen_name: TWITTER_SCREEN_NAME,
       include_rts: true,
       count: 2
     });
 
-  }).then((data) => {
-
-    return poll(
+    
+    await poll(
       data[process.env.MODE == 'prod' ? 0 : 1].id_str,
       logger,
       slackClient,
       twitterClient);
 
-  }).catch((err) => {
+  } catch (e) {
 
     logger.error(err, Array.from(arguments).slice(1));
 
-  });
+  }
 
   // Start server
   server.start();
 
 }
 
-function poll(sinceId, logger, slackClient, twitterClient) {
+async function poll(sinceId, logger, slackClient, twitterClient) {
 
-  const POLL_INTERVAL = 30 * 1000;
+  const POLL_INTERVAL = (process.env.mode == 'prod') ? 30 * 1000 : 10 * 1000;
 
-  return new Promise((resolve, reject) => {
-
+  await new Promise((resolve, reject) => {
     setTimeout(resolve, POLL_INTERVAL);
-
-  }).then(() => {
-
-    return twitterClient.getUserTimeline({
-      screen_name: TWITTER_SCREEN_NAME,
-      include_rts: true,
-      since_id: sinceId
-    });
-
-  }).then((data) => {
-
-    if (data.length) {
-      sinceId = data[0].id_str;
-      return slackClient.publish(data);
-    }
-
-  }).then(() => {
-    
-    return poll(sinceId, logger, slackClient, twitterClient);
-
   });
+
+  const data = await twitterClient.getUserTimeline({
+    screen_name: TWITTER_SCREEN_NAME,
+    include_rts: true,
+    since_id: sinceId
+  });
+
+  if (data.length) {
+    sinceId = data[0].id_str;
+    slackClient.publish(data);
+  }
+    
+  return poll(sinceId, logger, slackClient, twitterClient);
 
 }
 
