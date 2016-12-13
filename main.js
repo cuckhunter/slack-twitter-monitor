8e3b9771109edddb11c431d566fa4fb6520777d9
Chiraag Mundhe <cmundhe@gmail.com>
2016-12-13 04:54:29 -0500

    Add env logging

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ async function main() {
     
     const HOSTNAME = $env.HOSTNAME;
     const PORT = $env.PORT || 80;
+    console.dir($env);
     $server.listen(PORT, () => {
       $logger.info(`Server running at http://${HOSTNAME}:${PORT}/`);
     });
