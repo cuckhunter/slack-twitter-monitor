9b2dfc5f92e05bfe890978d7357bf99aa14718d0
Chiraag Mundhe <cmundhe@gmail.com>
2016-12-13 04:48:26 -0500

    Dependency injection + refactor + best tweets task

--- main.js
+++ main.js
@@ -1,68 +1,32 @@
-/* jshint camelcase: false */
-
 'use strict';
 
-require('dotenv').config();
-const lib = require('./lib');
-
-const TWITTER_SCREEN_NAME = 
-'realDonaldTrump'
-//'notiven'
-;
+const inject = require('./lib/inject');
 
 async function main() {
 
-  const logger = lib.getLogger();
-  const twitterClient = lib.getTwitterClient(logger);
-  const slackClient = lib.getSlackClient(logger);
-  const server = lib.getServer(logger, slackClient);
-
-  // Start server
-  server.start();
-
-  try {
- 
-    const data = await twitterClient.getUserTimeline({
-      screen_name: TWITTER_SCREEN_NAME,
-      include_rts: true,
-      count: 2
+  inject.ready();
+  
+  await inject(async function([$env, $logger, $server, $taskService]) {
+    
+    const PORT = $env.PORT || 80;
+    $server.listen(PORT, () => {
+      $logger.info(`Server running at http://localhost:${PORT}/`);
     });
-    
-    await poll(
-      data[process.env.MODE == 'prod' ? 0 : 1].id_str,
-      logger,
-      slackClient,
-      twitterClient);
 
-  } catch (e) {
+    const [i, j] = ($env.MODE == 'prod') ?
+      [30 * 1000, 120 * 60 * 1000] :
+      [10 * 1000, 60 * 1000];
 
-    logger.error(err, Array.from(arguments).slice(1));
+    await Promise.all([
+      $taskService.runAllTweetsTask('realDonaldTrump', i),
+      $taskService.runBestTweetsTask('Phi_Delta_Alpha', j)
+    ]);
 
-  }
-
-}
-
-async function poll(sinceId, logger, slackClient, twitterClient) {
-
-  const POLL_INTERVAL = (process.env.mode == 'prod') ? 30 * 1000 : 10 * 1000;
-
-  await new Promise((resolve, reject) => {
-    setTimeout(resolve, POLL_INTERVAL);
-  });
-
-  const data = await twitterClient.getUserTimeline({
-    screen_name: TWITTER_SCREEN_NAME,
-    include_rts: true,
-    since_id: sinceId
-  });
-
-  if (data.length) {
-    sinceId = data[0].id_str;
-    await slackClient.publish(data);
-  }
-    
-  return poll(sinceId, logger, slackClient, twitterClient);
+  })();
 
 }
 
-main();
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
