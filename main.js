4dfdee65463127dacf0c12f2c6259531482d33df
Chiraag Mundhe <cmundhe@gmail.com>
2016-12-17 17:29:26 -0500

    Refactor into app.js

--- main.js
+++ main.js
@@ -1,33 +1,21 @@
 'use strict';
 
-const inject = require('./lib/inject');
-
 async function main() {
 
-  inject.ready();
-  
-  await inject(async function([$env, $logger, $server, $taskService]) {
+  try {
+
+    const inject = require('./lib/inject');
+    const app = require('./lib/app');
+    inject.ready();
+    await app();
+
+  } catch (e) {
     
-    const PORT = $env.PORT || 80;
+    console.error(e);
+    process.exit(1);
 
-    $server.listen(PORT, () => {
-      $logger.info(`Server running at http://localhost:${PORT}/`);
-    });
-
-    const [i, j] = ($env.MODE == 'prod') ?
-      [30 * 1000, 120 * 60 * 1000] :
-      [10 * 1000, 60 * 1000];
-
-    await Promise.all([
-      $taskService.runAllTweetsTask('realDonaldTrump', i),
-      $taskService.runBestTweetsTask('Phi_Delta_Alpha', j)
-    ]);
-
-  })();
+  }
 
 }
 
-main().catch((e) => {
-  console.error(e);
-  process.exit(1);
-});
+main();
