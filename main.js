301f8573dcad58b97e2cefcd5c5cc2d21d3b867f
cuckhunter <cmundhe@gmail.com>
2019-03-13 13:36:53 -0400

    Changes

--- main.js
+++ main.js
@@ -1,56 +1,56 @@
 'use strict';
 
-let active = false;
+// let active = false;
 
-process.on('uncaughtException', async function(error) {
+// process.on('uncaughtException', async function(error) {
  
-  console.log('in uncaughtException:', error);
+//   console.log('in uncaughtException:', error);
 
-  try {
+//   try {
 
-    if (active) {
-      console.error('Error-handling error:', error);
-      process.exit(1);
-    }
+//     if (active) {
+//       console.error('Error-handling error:', error);
+//       process.exit(1);
+//     }
 
-    active = true;
+//     active = true;
 
-    console.error('Uncaught error:', error);
+//     console.error('Uncaught error:', error);
 
-    const request = require('request');
-    const SLACK_CHANNEL = '#slack-twitter-monitor';
-    const params = {
-      url: process.env.SLACK_API_ENDPOINT,
-      body: JSON.stringify({
-        channel: SLACK_CHANNEL,
-        username: 'twitter',
-        text: `<@U24PPP3BK> 🌎 Twitter bot is kill 🌎\n\n\`\`\`${e.stack}\`\`\``,
-        icon_url: 'https://a.slack-edge.com/66f9/img/services/twitter_36.png'
-        //icon_emoji: ''
-      })
-    };
+//     const request = require('request');
+//     const SLACK_CHANNEL = '#slack-twitter-monitor';
+//     const params = {
+//       url: process.env.SLACK_API_ENDPOINT,
+//       body: JSON.stringify({
+//         channel: SLACK_CHANNEL,
+//         username: 'twitter',
+//         text: `<@U24PPP3BK> 🌎 Twitter bot is kill 🌎\n\n\`\`\`${e.stack}\`\`\``,
+//         icon_url: 'https://a.slack-edge.com/66f9/img/services/twitter_36.png'
+//         //icon_emoji: ''
+//       })
+//     };
 
-    await new Promise((resolve, reject) => {
+//     await new Promise((resolve, reject) => {
 
-      request.post(params, (err, response, body) => {
-        if (err) {
-          reject(new Error(JSON.stringify({err, response, body})));
-        } else {
-          resolve(response, body);
-        }
-      });
+//       request.post(params, (err, response, body) => {
+//         if (err) {
+//           reject(new Error(JSON.stringify({err, response, body})));
+//         } else {
+//           resolve(response, body);
+//         }
+//       });
 
-    });
+//     });
 
-    console.log(`Reported error to Slack channel ${SLACK_CHANNEL}`);
+//     console.log(`Reported error to Slack channel ${SLACK_CHANNEL}`);
 
-  } catch (e) {
-    console.log('Error-handling error:', e);
-  } finally {
-    process.exit(1);
-  }
+//   } catch (e) {
+//     console.log('Error-handling error:', e);
+//   } finally {
+//     process.exit(1);
+//   }
 
-});
+// });
 
 function main() {
 
