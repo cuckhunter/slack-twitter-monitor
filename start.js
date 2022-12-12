6ad1621ab07af304718efd542f833f3c01970120
cuckhunter <cmundhe+github@mail.com>
2022-12-12 09:36:05 -0500

    Changes

--- start.js
+++ start.js
@@ -8,28 +8,28 @@ let redeployInterval = 2;
 
 function main() {
 
-	const child = fork('main', ['--harmony'], { stdio: 'pipe' });
+	// const child = fork('main', ['--harmony'], { stdio: 'pipe' });
 
-	child.on('close', (code, signal) => {
+	// child.on('close', (code, signal) => {
 
-		if (redeploys == REDEPLOY_LIMIT) {
-			console.log('Reached deployment limit');
-			process.exit(1);
-		}
+	// 	if (redeploys == REDEPLOY_LIMIT) {
+	// 		console.log('Reached deployment limit');
+	// 		process.exit(1);
+	// 	}
 
-		redeployInterval = Math.round(redeployInterval * 1.5 ** redeploys++);
-		console.log(`Redeploy attempt #${redeploys} in ${redeployInterval} seconds...`);
-		setTimeout(main, redeployInterval * 1000);
+	// 	redeployInterval = Math.round(redeployInterval * 1.5 ** redeploys++);
+	// 	console.log(`Redeploy attempt #${redeploys} in ${redeployInterval} seconds...`);
+	// 	setTimeout(main, redeployInterval * 1000);
 
-	});
+	// });
 
-	child.stdout.on('data', (data) => {
-	  process.stdout.write(`stdout: ${data}`); // use instead of console to eliminate trailing newline
-	});
+	// child.stdout.on('data', (data) => {
+	//   process.stdout.write(`stdout: ${data}`); // use instead of console to eliminate trailing newline
+	// });
 
-	child.stderr.on('data', (data) => {
-	  process.stderr.write(`stderr: ${data}`); // use instead of console to eliminate trailing newline
-	});
+	// child.stderr.on('data', (data) => {
+	//   process.stderr.write(`stderr: ${data}`); // use instead of console to eliminate trailing newline
+	// });
 
 }
 
